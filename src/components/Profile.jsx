b5531eaa26c8cb9419888e1c4244e4863b220655
Ryan Young <frostwow13@yandex.ru>
2020-12-24 22:00:34 +0300

    Complete a first edition of HTML code

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className='content'>
+
+
+
+    <div className='profilePicture'>
+      <img src='https://4.bp.blogspot.com/-TLaoMfZtF7Q/Vp3iWrZf7wI/AAAAAAAA8WA/-RPPQ58sTx8/s1600/Kaplan-hd-wallpapers%2B%2528165%2529.jpg' />
+      <p>Download new picture</p>
+    </div>
+
+    <div>
+      <h3>Ruslan Yusupov</h3>
+    </div>
+    <div className="description">
+      <p>Date of Brith: 28 June</p>
+      <p>City: Tula</p>
+      <p>Education: TulGU</p>
+      <p className="lastFeature">Web site: https://it-social-network.com</p>
+    </div>
+
+
+  </div>
+}
+
+export default Profile;
---
+++ src/components/posts.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Posts = () => {
+  return <div className="posts">
+
+    <div>
+    <h4>New Post</h4>
+    </div>
+
+    <div className="textArea">
+      <p>Type something new here...</p>
+    </div>
+
+    <div>
+      <button type="button" name="button">Submit</button>
+    </div>
+
+    <div className="firstPost post">
+      <h3>Day 2</h3>
+      <p>Tortor condimentum lacinia quis vel eros donec ac. Porta non pulvinar neque laoreet suspendisse interdum consectetur libero. Non pulvinar neque laoreet suspendisse interdum consectetur libero. Mauris ultrices eros in cursus turpis massa tincidunt. Dignissim suspendisse in est ante in nibh mauris cursus mattis. Neque aliquam vestibulum morbi blandit cursus risus at ultrices mi. Ultricies mi quis hendrerit dolor magna eget est lorem. Fermentum et sollicitudin ac orci phasellus. Ornare lectus sit amet est placerat in. Nunc lobortis mattis aliquam faucibus purus in massa tempor. Diam sit amet nisl suscipit adipiscing bibendum est ultricies integer.</p>
+    </div>
+
+    <div className="post">
+      <h3>Day 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Erat nam at lectus urna duis. Nulla facilisi morbi tempus iaculis urna id volutpat lacus. Vel pretium lectus quam id leo. Massa tempor nec feugiat nisl pretium. Cras sed felis eget velit aliquet sagittis. Sed felis eget velit aliquet. Vitae nunc sed velit dignissim sodales. Egestas fringilla phasellus faucibus scelerisque. Vulputate sapien nec sagittis aliquam. Parturient montes nascetur ridiculus mus mauris. Tincidunt vitae semper quis lectus nulla at volutpat diam.</p>
+    </div>
+
+  </div>
+}
+
+export default Posts;
